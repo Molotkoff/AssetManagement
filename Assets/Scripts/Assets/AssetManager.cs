8879b08ac4da4b2d53103904d7c54688b7ccf2c1
Kozloder <andrew.molotkoff@gmail.com>
2022-05-22 21:28:29 +0700

    MVP

---
+++ Assets/Scripts/Assets/AssetManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using Unity.Burst;
+
+namespace Game.Assets
+{
+    [CreateAssetMenu(menuName = "Game/Assets/Manager/Create")]
+    public class AssetManager : ScriptableSingleton<AssetManager>
+    {
+        [SerializeField] private BaseAssetManager[] _assetsManagers;
+        private Dictionary<Type, BaseAssetManager> _assetsManagersCache;
+        
+        public R Create<S, R>(S settings)
+        {
+            var type = typeof(S);
+
+            if (_assetsManagersCache == null)
+                _assetsManagersCache = new Dictionary<Type, BaseAssetManager>();
+
+            if (!_assetsManagersCache.TryGetValue(type, out var manager))
+            {
+                foreach (var _assetManager in _assetsManagers) 
+                {
+                    if (_assetManager is AssetManager<S, R>)
+                    {
+                        manager = _assetManager;
+                        _assetsManagersCache.Add(type, manager);
+                        break;
+                    }
+                }
+            }
+
+#if UNITY_EDITOR
+            Debug.Assert(manager != null, $"No defined asset-manager of type:{type}");
+#endif
+            var assetManager = (AssetManager<S, R>)manager;
+            return assetManager.Create(settings);
+        }
+
+#if UNITY_EDITOR
+        public static bool Save()
+        {
+            var instance = AssetManager.instance;
+
+            if (instance != null)
+            {
+                instance.Save(true);
+                return true;
+            }
+
+            return false;
+        }
+#endif
+
+        [MenuItem("Game/Test")]
+        public static void Test()
+        {
+
+        }
+
+    }
+
+    [Serializable]
+    public class BaseAssetManager { }
+
+    [Serializable]
+    public abstract class AssetManager<S, R> : BaseAssetManager
+    {
+        public abstract R Create(S settings);
+    }
+}
---
+++ Assets/Scripts/Editor/AssetManagerPostProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Game.Assets
+{
+    class AssetManagerPostProcessor : AssetPostprocessor
+    {
+        public static void OnPostprocessAllAssets(System.String[] importedAssets, System.String[] deletedAssets, System.String[] movedAssets, System.String[] movedFromAssetPaths)
+        {
+            foreach (var importedAsset in importedAssets)
+            {
+                var asset = AssetDatabase.LoadAssetAtPath<ScriptableObject>(importedAsset);
+                
+                if (asset != null)
+                {
+                    var assetType = asset.GetType();
+                    if (assetType.IsDefined(typeof(RequiredAssetAttribute), false)) //is reauired-asset
+                    {
+                        AssetManagerEditor.ConfigureRequiredAsset(asset);
+                    }
+                }
+            }
+        }
+    }
+}
